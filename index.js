19fc475b7795be81f637e6a731c61f7b1a3beff5
Md. Fawjul Azim <mdfawjulazim617@gmail.com>
2024-09-15 00:50:26 +0600

    answer 5

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-
+console.log("Answers to the questions 1")
 /*1) Write a function named calculateDifference that takes two arguments and returns the difference between the first and second argument.*/
 function calculateDifference(a, b) {
     return a - b;
@@ -7,7 +7,7 @@ const result = calculateDifference(10, 5);
 console.log(result); 
 
 
-
+console.log("Answers to the questions 2")
 /*2) Write a function named isOdd that takes one argument and returns true if the number is odd, and false if it is not. */
 function isOdd(num) {
     return num % 2 !== 0;
@@ -17,6 +17,8 @@ let ress = isOdd(9);
 console.log(res);  
 console.log(ress);  
 
+
+console.log("Answers to the questions 3")
 /* 3) Write a function named findMin that takes an array of numbers and returns the smallest number in the array.*/
 function findMin(arr) {
     return Math.min(...arr);
@@ -24,6 +26,8 @@ function findMin(arr) {
 const numbers = [5, 2, 9,0, 1, 7];
 const minNumber = findMin(numbers);
 console.log(minNumber);
+
+console.log("Answers to the questions 4")
 /*4) Write a function named filterEvenNumbers that takes an array of numbers and returns a new array containing only the even numbers. */
 function filterEvenNumbers(arr) {
     return arr.filter(num => num % 2 === 0);
@@ -34,6 +38,19 @@ let arr2 = [10, 15, 20, 25, 30]
 let arr2_res =filterEvenNumbers(arr2);
 console.log(arr1_res);  
 console.log(arr2_res); 
+
+
+console.log("Answers to the questions 5")
+/*5) Write a function named findMax that takes an array of numbers and returns the largest number in the array. */
+function sortArrayDescending(arr) {
+    return arr.slice().sort((a, b) => b - a);
+}
+let arr3 = [1, 2, 3, 4, 5, 6]
+let arr3_res =sortArrayDescending(arr3);
+let arr4 = [10, 15, 20, 25, 30]
+let arr4_res =sortArrayDescending(arr4);
+console.log(arr3_res);  
+console.log(arr4_res); 
 
 
 
